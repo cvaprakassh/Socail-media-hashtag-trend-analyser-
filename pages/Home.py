2428791b1a838283bccbcb8e9db153b71d1bc3e8
Nandhini Prabha <sivaprakash@Nandhinis-MacBook-Air.local>
2025-01-21 15:34:51 +0530

    post api integrated api changes

--- pages/Home.py
+++ pages/Home.py
@@ -18,20 +18,20 @@ st.markdown(
 )
 st.title("Social Media Hashtag Trend Analyser")
 st.page_link("SMedia.py", label="Home", icon="🏠")
+# Function to submit a post to the API
+def post_content(post_text):
+    url = "https://go07cpukrh.execute-api.ap-south-1.amazonaws.com/Post-Stage"  # API endpoint
+    payload = {"content": post_text}
+    headers = {"Content-Type": "application/json"}
+        
+    response = requests.post(url, json=payload, headers=headers)
+        
+    if response.status_code == 201:
+        st.success("Post successfully submitted!")
+    else:
+        st.error("Failed to submit post. Please try again.")
+
 post_text= st.text_area("Enter your post here","",placeholder="#Tag1 #Tag2 #Tag3 , Post")
 if st.button("Post"):
     post_content(post_text)
-    
-# Function to submit a post to the API
-    def post_content(post_text):
-        url = "https://example.com/api/posts"  # Replace with your API endpoint
-        payload = {"content": post_text}
-        headers = {"Content-Type": "application/json"}
-        
-        response = requests.post(url, json=payload, headers=headers)
-        
-        if response.status_code == 201:
-            st.success("Post successfully submitted!")
-        else:
-            st.error("Failed to submit post. Please try again.")
 
