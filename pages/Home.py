896c6b43d88e18f30614a531d2c7fe3cfc1747ef
Nandhini Prabha <sivaprakash@Nandhinis-MacBook-Air.local>
2025-01-22 15:03:32 +0530

    post api data added

--- pages/Home.py
+++ pages/Home.py
@@ -1,5 +1,24 @@
 import streamlit as st
 import requests
+import re
+import json
+
+#create id auto increment
+def get_next_id():
+    try:
+        with open("id_counter.json", "r") as file:
+            data = json.load(file)
+            current_id = data["current_id"]
+    except FileNotFoundError:
+        current_id = 0
+
+    next_id = current_id + 1
+
+    with open("id_counter.json", "w") as file:
+        json.dump({"current_id": next_id}, file)
+
+    return next_id
+
 st.set_page_config(
     page_title="Social Media Hashtag Trend Analyser",
     page_icon=":hash:",
@@ -19,7 +38,7 @@ st.markdown(
 st.title("Social Media Hashtag Trend Analyser")
 st.page_link("SMedia.py", label="Home", icon="🏠")
 # Function to submit a post to the API
-def post_content(post_text):
+def post_info(post_text):
     url = "https://go07cpukrh.execute-api.ap-south-1.amazonaws.com/Post-Stage"  # API endpoint
     payload = {"content": post_text}
     headers = {"Content-Type": "application/json"}
@@ -31,7 +50,12 @@ def post_content(post_text):
     else:
         st.error("Failed to submit post. Please try again.")
 
-post_text= st.text_area("Enter your post here","",placeholder="#Tag1 #Tag2 #Tag3 , Post")
+post_cont= st.text_area("Enter your post here","",placeholder="#Tag1 #Tag2 #Tag3 , Post")
+post_tags = re.findall(r"#(\w+)", post_cont)  # Extract hashtags from post content
+post_desc = re.sub(r"#(\w+)", "", post_cont).strip()  # Extract post description
+post_id = get_next_id()  # Get the next post ID
+post_text=json.dumps({"id": post_id, "description": post_desc, "tags": post_tags}) # Create a JSON object 
+
 if st.button("Post"):
-    post_content(post_text)
+    post_info(post_text)
 
