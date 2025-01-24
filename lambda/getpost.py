b4927e90ccb2408a704a11912b310f6da64d830b
Nandhini Prabha <sivaprakash@Nandhinis-MacBook-Air.local>
2025-01-24 14:05:20 +0530

    post from api added

--- lambda/getpost.py
+++ lambda/getpost.py
@@ -5,12 +5,9 @@ def lambda_handler(event, context):
      dynamodb = boto3.resource('dynamodb')
      table = dynamodb.Table('tb_post')
      response = table.scan()
-     data = response['Items']
-     while 'LastEvaluatedKey' in response:
-        response = table.scan(ExclusiveStartKey=response['LastEvaluatedKey'])
-        data.extend(response['Items'])
-        return {
+     items = response['Items']
+     return {
             'statusCode': 200,
-            'body': json.dumps(data)
+            'body': json.dumps(items)
         }
 
--- pages/Post.py
+++ pages/Post.py
@@ -11,3 +11,20 @@ st.markdown(
 )
 st.title("Posts")
 st.page_link("SMedia.py", label="Home", icon="🏠")
+
+# Function to fetch posts from an API
+def fetch_posts():
+    url = "https://go07cpukrh.execute-api.ap-south-1.amazonaws.com/Post-Stage"
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        return response.json()
+    except requests.exceptions.RequestException as e:
+        st.error(f"Error fetching posts: {e}")
+        return []
+# Display posts 
+posts = fetch_posts()
+if posts:
+    st.write(posts)
+else:
+    st.write("No posts available at the moment.")
