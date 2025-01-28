ba0c46b265923026b1c1403898362bee8c41f4df
Nandhini Prabha <sivaprakash@Nandhinis-MacBook-Air.local>
2025-01-28 15:09:26 +0530

    post  api fixes

--- pages/Home.py
+++ pages/Home.py
@@ -37,16 +37,45 @@ st.markdown(
 )
 st.title("Social Media Hashtag Trend Analyser")
 st.page_link("SMedia.py", label="Home", icon="🏠")
+
+# Function to check payload
+def check_payload(payload):
+    if "postid" not in payload:
+        st.error("Post ID is missing.")
+        return False
+    if "post_content" not in payload:
+        st.error("Post content is missing.")
+        return False
+    if "tags" not in payload:
+        st.error("Tags are missing.")
+        return False
+    try:
+        json.loads(json.dumps(payload))
+    except ValueError as e:
+        st.error("Payload is not a valid JSON.")
+        return False
+    return True
+
+
+
 # Function to submit a post to the API
 def post_info(post_text):
     url = "https://go07cpukrh.execute-api.ap-south-1.amazonaws.com/Post-Stage"  # API endpoint
-    method = "POST"
-    payload = {"content": post_text}
-    headers = {"Content-Type": "application/json"}
+    payload = json.loads(post_text)  # Convert JSON string to dictionary
+    #check_payload(payload)
+    
+    try:    
+        response = requests.post(url, json=payload)
+        response.raise_for_status()
+    except requests.exceptions.RequestException as e:
+        st.error(f"Error submitting post: {e}")
+        return
+    #st.success("Post successfully submitted!")
+    #st.write(response.text)
+    #st.write(response.status_code)
+    #st.write(response.json())
         
-    response = requests.request(method, url, headers=headers, json=payload)
-        
-    if response.status_code == 201:
+    if response.status_code == 200:
         st.success("Post successfully submitted!")
     else:
         st.error("Failed to submit post. Please try again.")
@@ -55,10 +84,11 @@ def post_info(post_text):
 post_cont= st.text_area("Enter your post here","",placeholder="#Tag1 #Tag2 #Tag3 , Post")
 post_tags = re.findall(r"#(\w+)", post_cont)  # Extract hashtags from post content
 post_desc = re.sub(r"#(\w+)", "", post_cont).strip()  # Extract post description
-post_id = get_next_id()  # Get the next post ID
+post_id = get_next_id() # Get the next post ID
 #st.write(type(post_id))
-post_text=json.dumps({'postid': post_id, "post_content": post_desc, "tags": post_tags}) # Create a JSON object 
-st.write(post_text)
+post_text=json.dumps({"postid":post_id,"post_content": post_desc, "tags": post_tags}) # Create a JSON object 
+#st.write(post_text)
 if st.button("Post"):
     post_info(post_text)
+
 
